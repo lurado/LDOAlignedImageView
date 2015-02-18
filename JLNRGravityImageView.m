de6a32118f806febc52fd91dee1ecca6d911caad
Julian Raschke <julian@raschke.de>
2015-02-18 14:59:59 +0700

    Implement alignRight as proof of concept

--- JLNRGravityImageView.m
+++ JLNRGravityImageView.m
@@ -11,26 +11,74 @@
 
 @implementation JLNRGravityImageView
 
+#pragma mark - Custom setters
+
+- (void)setAlignTop:(BOOL)alignTop
+{
+    _alignTop = alignTop;
+    
+    [self calculateContentsRect];
+}
+
+- (void)setAlignBottom:(BOOL)alignBottom
+{
+    _alignBottom = alignBottom;
+    
+    [self calculateContentsRect];
+}
+
+- (void)setAlignLeft:(BOOL)alignRight
+{
+    _alignRight = alignRight;
+    
+    [self calculateContentsRect];
+}
+
+- (void)setAlignRight:(BOOL)alignRight
+{
+    _alignRight = alignRight;
+    
+    [self calculateContentsRect];
+}
+
+#pragma mark - UIView
+
 - (void)layoutSubviews
 {
     [super layoutSubviews];
     
-    switch (self.contentMode) {
-        case UIViewContentModeBottom:
-        case UIViewContentModeBottomLeft:
-        case UIViewContentModeBottomRight:
-        case UIViewContentModeCenter:
-        case UIViewContentModeLeft:
-        case UIViewContentModeRedraw:
-        case UIViewContentModeRight:
-        case UIViewContentModeScaleAspectFill:
-        case UIViewContentModeScaleAspectFit:
-        case UIViewContentModeScaleToFill:
-        case UIViewContentModeTop:
-        case UIViewContentModeTopLeft:
-        case UIViewContentModeTopRight:
-            break;
+    [self calculateContentsRect];
+}
+
+- (void)setContentMode:(UIViewContentMode)contentMode
+{
+    [super setContentMode:contentMode];
+    
+    [self calculateContentsRect];
+}
+
+- (void)calculateContentsRect
+{
+    CGRect contentsRect = CGRectMake(0, 0, 1, 1);
+
+    if (self.contentMode != UIViewContentModeScaleAspectFill || self.image == nil) {
+        self.layer.contentsRect = contentsRect;
+        return;
     }
+    
+    CGSize imageSize = self.image.size; // 1337x999
+    CGSize frameSize = self.frame.size; // 288x381
+    
+    CGFloat scaleFactor = MAX(frameSize.width / imageSize.width, frameSize.height / imageSize.height); // MAX(0.215, 0.381) = 0.381
+    
+    CGSize scaledImageSize = CGSizeMake(imageSize.width * scaleFactor, imageSize.height * scaleFactor);
+    
+    if (self.alignRight) {
+        CGFloat newScaledImageWidth = scaledImageSize.width + (scaledImageSize.width - frameSize.width);
+        contentsRect.size.width = newScaledImageWidth / scaledImageSize.width;
+    }
+    
+    self.layer.contentsRect = contentsRect;
 }
 
 @end
